208407bb6917f08609648ae8b69249a8a4ad083d
Alisahib Amiraslan <=>
2022-05-05 03:48:11 +0300

    create new general questions

---
+++ app.js
@@ -0,0 +1,34 @@
+// TRAVERSİNG DOM 
+
+/* const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach((btn) =>{
+	btn.addEventListener('click', (e) =>{
+		const question = e.currentTarget.parentElement.parentElement;
+		question.classList.toggle("show-text");
+	});
+}); */
+
+
+// Selectors
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach( function(question){
+	// change to article and try it work or not e.x const btn = question
+   const btn = question;
+   // .querySelector(".question-btn");
+   
+
+
+   btn.addEventListener('click', () =>{
+   	questions.forEach((item) =>{
+   		if (item !==question) {
+   			item.classList.remove('show-text')
+   		}
+   	})
+
+   	question.classList.toggle("show-text");
+   })
+
+});
